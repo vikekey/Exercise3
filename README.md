29a1c5d762b6661fc904ca6b4c17b8ae353107b5
vikekey <vickywallynjoseph@gmail.com>
2024-02-05 20:41:07 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Exercise3
+
+"you'll make a good girl crazy if you don't treat her like a lady!
+"
+
+- Celine Dion
---
+++ index.js
@@ -0,0 +1,7 @@
+//created quote app
+let quotes = [
+  "Welcome to Good Burger, Home of the Good Burger, can I take your order - Ed",
+  "Why you bother me when you know you don't want me? - SZA",
+  "What? Like it's hard?- Elle Woods",
+];
+console.log(quotes[Math.floor(Math.random() * 3)]);
